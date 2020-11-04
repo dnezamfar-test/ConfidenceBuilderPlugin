19f257af2d7a7f3bc31323f6ec621f6461081b6f
Brennan Beam <BrennanBeam@gmail.com>
2020-11-04 14:24:51 -0600

    Create Normal Distribution Class and move GetInvCDF there.

--- src/ConfidenceBuilderPlugin/Line.java
+++ src/ConfidenceBuilderPlugin/Line.java
@@ -30,26 +30,6 @@ public class Line {
             points.remove(minIndex);
         }
     }
-
-    private double GetInvCDF(double probability){
-        // Stolen from Will's statistics Library //Create normal distribution class. Does not belong in line.
-        double zScore;
-        //Taylor Series Coefficients
-        double c0 = 2.515517;
-        double c1 = 0.802853;
-        double c2 = 0.010328;
-        double d1 = 1.432788;
-        double d2 = 0.189269;
-        double d3 = 0.001308;
-        //QC the input
-        if(probability == 0.5) { zScore = 0; return zScore;}
-        if(probability <= 0) {probability = 0.0000000000000001;}
-        if(probability >= 1) {probability = 0.9999999999999999;}
-        //Conversion happens here
-        double t = Math.sqrt(Math.log(1/Math.pow(probability,2)));
-        zScore = t - (c0+c1*t+c2*Math.pow(t,2)) / (1+d1*t+d2*Math.pow(t,2)+d3*Math.pow(t,3));
-        return zScore;
-    }
     private double PerpendicularDistance(Point lineStart, Point testPoint, Point lineEnd){
         double area = Math.abs(0.5*(lineStart.getX()*(testPoint.getY()-lineEnd.getY()) + testPoint.getX() * (lineEnd.getY()-lineStart.getY()) + lineEnd.getX() * (lineStart.getY() - testPoint.getY())));
         double base = Math.sqrt(Math.pow(lineStart.getX()-testPoint.getX(),2)+Math.pow(lineStart.getY()-testPoint.getY(),2));
