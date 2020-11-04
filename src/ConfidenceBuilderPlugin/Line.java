6c31dfe00be150735fb3c2e8ec1e7e9e3cea1017
Brennan Beam <BrennanBeam@gmail.com>
2020-11-04 14:35:53 -0600

    Move Perpendicular Distance method from line to point.

--- src/ConfidenceBuilderPlugin/Line.java
+++ src/ConfidenceBuilderPlugin/Line.java
@@ -30,9 +30,4 @@ public class Line {
             points.remove(minIndex);
         }
     }
-    private double PerpendicularDistance(Point lineStart, Point testPoint, Point lineEnd){
-        double area = Math.abs(0.5*(lineStart.getX()*(testPoint.getY()-lineEnd.getY()) + testPoint.getX() * (lineEnd.getY()-lineStart.getY()) + lineEnd.getX() * (lineStart.getY() - testPoint.getY())));
-        double base = Math.sqrt(Math.pow(lineStart.getX()-testPoint.getX(),2)+Math.pow(lineStart.getY()-testPoint.getY(),2));
-        return (area/base * 2); //height
-    }
 }
--- src/ConfidenceBuilderPlugin/Point.java
+++ src/ConfidenceBuilderPlugin/Point.java
@@ -16,5 +16,10 @@ public class Point {
         _x = x;
         _y = y;
     }
+    private double PerpendicularDistance(Point lineStart, Point testPoint, Point lineEnd){
+        double area = Math.abs(0.5*(lineStart.getX()*(testPoint.getY()-lineEnd.getY()) + testPoint.getX() * (lineEnd.getY()-lineStart.getY()) + lineEnd.getX() * (lineStart.getY() - testPoint.getY())));
+        double base = Math.sqrt(Math.pow(lineStart.getX()-testPoint.getX(),2)+Math.pow(lineStart.getY()-testPoint.getY(),2));
+        return (area/base * 2); //height
+    }
 }
 
