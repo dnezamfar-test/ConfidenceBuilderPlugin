91c0b53032576cf0031f09dae8c57b8fa14ac19b
Brennan Beam <BrennanBeam@gmail.com>
2020-11-04 09:44:40 -0600

    Removed Duplicate VW Method. Corrected some translation errors in the remaining VW method.

--- src/ConfidenceBuilderPlugin/Line.java
+++ src/ConfidenceBuilderPlugin/Line.java
@@ -3,40 +3,26 @@ import java.util.ArrayList;
 import java.lang.Math;
 
 public class Line {
-    private ArrayList<Point> points;
-
+    //Variables
+    public ArrayList<Point> points;
+    //Constructor
     public Line(double[] x, double[] y) {
         for(int i=0; i < x.length ; i++){
             Point pt = new Point(x[i],y[i]);
             points.add(pt); }
     }
+    //Methods
     public void  VisvaligamWhyattSimplify(int numToKeep) {
         //Thins a line down to the specified amount of points based on the VW algorithm.
         //Implementation based on the description of the method here: http://bost.ocks.org/mike/simplify/
         int removeLimit = (points.size()) - numToKeep;
+        int minIndex = 1;
         for(int i = 0; i < removeLimit; i++) {
-            int minIndex;
             float minArea = Area(points.get(0), points.get(1), points.get(2)); // This is the baseline we'll start our first comparison to.
             for(int j=2; j< points.size()-2;j++) // starting at 2, because we're gonna start calculating areas, and we already calculated area 1.
             { //ending at minus 2 because we don't want the last point, and the size property uses counting numbers, so we have to reduce by an extra -1 to account for 0 index
                 float tmpArea = Area(points.get(j-1), points.get(j), points.get(j+1));
                 if (tmpArea < minArea) {
-                    minIndex = j;
-                    minArea = tmpArea;
-                    points.remove(minIndex);
-                }
-            }
-        }
-    }
-    public void VisvaligamWhyattSimplifyWill(int numToKeep){
-        //Stolen from Will's function in LifeSimGIS by the same name. Thins a line down to the specified amount of points based on the VW algorithm.
-        int removeLimit = points.size()-numToKeep;
-        int minIndex = 1;
-        for(int i=0; i<removeLimit; i++){
-            double minArea = Area(points.get(0), points.get(1), points.get(2) );
-            for(int j=2; j< points.size() - 2; j++){
-                double tmpArea = Area(points.get(j-1), points.get(j), points.get(j+1) );
-                if(tmpArea < minArea){
                     minIndex = j;
                     minArea = tmpArea;
                 }
