ccee8f51813abf59b8e98b5d287575d754e430cd
Brennan Beam <BrennanBeam@gmail.com>
2020-11-04 18:42:45 -0600

    Added unit test for Point and Line classes

--- src/ConfidenceBuilderPlugin/Line.java
+++ src/ConfidenceBuilderPlugin/Line.java
@@ -1,10 +1,11 @@
 package ConfidenceBuilderPlugin;
 import java.util.ArrayList;
-import java.lang.Math;
 
 public class Line {
     //Variables
     private ArrayList<Point> points;
+    //Getter
+    public ArrayList<Point> getPoints() { return points; }
     //Constructor
     public Line(double[] x, double[] y) {
         for(int i=0; i < x.length ; i++){
@@ -12,22 +13,23 @@ public class Line {
             points.add(pt); }
     }
     //Methods
-    public void  VisvaligamWhyattSimplify(int numToKeep) {
+    public void  VisvaligamWhyattSimplify(int numToKeep, ArrayList<Point> pointList) {
         //Thins a line down to the specified amount of points based on the VW algorithm.
         //Implementation based on the description of the method here: http://bost.ocks.org/mike/simplify/
-        int removeLimit = (points.size()) - numToKeep;
+
+        int removeLimit = (pointList.size()) - numToKeep;
         int minIndex = 1;
         for(int i = 0; i < removeLimit; i++) {
-            float minArea = Polygon.AreaOfTriangle(points.get(0), points.get(1), points.get(2)); // This is the baseline we'll start our first comparison to.
-            for(int j=2; j< points.size()-2;j++) // starting at 2, because we're gonna start calculating areas, and we already calculated area 1.
+            float minArea = Polygon.AreaOfTriangle(pointList.get(0), pointList.get(1), pointList.get(2)); // This is the baseline we'll start our first comparison to.
+            for(int j=2; j< pointList.size()-2;j++) // starting at 2, because we're gonna start calculating areas, and we already calculated area 1.
             { //ending at minus 2 because we don't want the last point, and the size property uses counting numbers, so we have to reduce by an extra -1 to account for 0 index
-                float tmpArea = Polygon.AreaOfTriangle(points.get(j-1), points.get(j), points.get(j+1));
+                float tmpArea = Polygon.AreaOfTriangle(pointList.get(j-1), pointList.get(j), pointList.get(j+1));
                 if (tmpArea < minArea) {
                     minIndex = j;
                     minArea = tmpArea;
                 }
             }
-            points.remove(minIndex);
+            pointList.remove(minIndex);
         }
     }
 }
--- test/ConfidenceBuilderPlugin/PointTest.java
+++ test/ConfidenceBuilderPlugin/PointTest.java
@@ -1,6 +1,14 @@
 package ConfidenceBuilderPlugin;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 class PointTest {
-  
+    Point pt0_0 = new Point(0,0);
+    Point pt1_1 = new Point(1,1);
+    Point pt2_4 = new Point(2,4);
+
+    @Test
+    void perpendicularDistance() {
+        assertEquals(Point.PerpendicularDistance(pt0_0, pt1_1, pt2_4), Math.sqrt(5)/5);
+    }
 }
