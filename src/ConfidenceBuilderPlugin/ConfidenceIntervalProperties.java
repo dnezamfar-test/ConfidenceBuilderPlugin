f0d28012cd6c113908f03ab99b4fca47373ac143
Brennan Beam <BrennanBeam@gmail.com>
2020-11-06 23:41:04 -0600

    Started Class to store properties file information.

---
+++ src/ConfidenceBuilderPlugin/ConfidenceIntervalProperties.java
@@ -0,0 +1,10 @@
+package ConfidenceBuilderPlugin;
+
+public class ConfidenceIntervalProperties {
+    private String _simulationName;
+    private Boolean _stochastic;
+    private double[] _xOrds;
+    private double _upperConfidenceBound;
+    private double _lowerConfidenceBound;
+
+}
--- test/ConfidenceBuilderPlugin/LineThinnerTest.java
+++ test/ConfidenceBuilderPlugin/LineThinnerTest.java
@@ -1,7 +1,5 @@
 package ConfidenceBuilderPlugin;
-
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class LineThinnerTest {
