1d19107f228da978f5bce132e99e9c78779cad9b
Brennan Beam <BrennanBeam@gmail.com>
2020-11-18 20:02:26 -0800

    Modifed PropertyFileReader to read weights information in the property file.

--- src/ConfidenceBuilderPlugin/PropertyFileReader.java
+++ src/ConfidenceBuilderPlugin/PropertyFileReader.java
@@ -5,13 +5,20 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class PropertyFileReader {
     public static Properties Read(String _propertiesFile) {
+        //variables
+        double endProb = 0;
+        List<Double> weights = new ArrayList<>();
+        double startProb = 0;
+        double incrementalWeightTemp;
         String _simulationName = "";
         ArrayList<Double> _xOrds = new ArrayList<>();
         ArrayList<Double> _CI_Values = new ArrayList<>();
         BufferedReader brp = null;
+
         try {
             brp = new BufferedReader(new FileReader(_propertiesFile));
             String[] tmp;
@@ -21,26 +28,30 @@ public class PropertyFileReader {
                 if (tmp.length == 0) {
                     continue;
                 }
-                switch (tmp[0]) {
-                    case "SimulationName" -> _simulationName = tmp[1];
-                    case "XOrds" -> {
-                        for (String s : tmp) {
-                            if (s.equals("XOrds")) {
-                                continue;
-                            }
+                if (tmp[0].equals("SimulationName") ){_simulationName = tmp[1];}
+                if (tmp[0] .equals( "XOrds")) {
+                    for (String s : tmp) {
+                            if (s.equals("XOrds")) { continue;}
                             _xOrds.add(1 - Double.parseDouble(s));
                         }
                     }
-                    case "CI_Vals" -> {
+                if (tmp[0] .equals( "CI_Vals")){
                         for (String s : tmp) {
-                            if (s.equals("CI_Vals")) {
-                                continue;
-                            }
+                            if (s.equals("CI_Vals")) { continue;}
                             _CI_Values.add(Double.parseDouble(s));
                         }
                     }
+                if (tmp[0].equals( "below")) {
+                        startProb = Double.parseDouble((tmp[1]));
+                    }
+                if(tmp[0] .equals( "above")){
+                        endProb = Double.parseDouble(tmp[1]);
+                    }
+                else{
+                        incrementalWeightTemp = Double.parseDouble(tmp[1]);
+                        weights.add(incrementalWeightTemp);
+                    }
                 }
-            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -54,8 +65,7 @@ public class PropertyFileReader {
                 }
             }
         }
-        Properties property = new Properties(_propertiesFile, _simulationName, _xOrds, _CI_Values);
-        return property;
+        return new Properties(_propertiesFile, _simulationName, _xOrds, _CI_Values);
     }
 }
 
